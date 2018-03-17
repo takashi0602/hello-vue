e9d6908de1f900335764368bd436fab499d0b57f
takashi <b7180@oic.jp>
2018-03-17 16:40:17 +0900

    todoリスト作成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <to-do/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import ToDo from './components/todo'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      ToDo
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/todo.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <form>
+            <input v-model="todo">
+            <button type="button" @click="sendTodo">追加</button>
+        </form>
+        <ul v-for="item in items">
+            <li @click="doneTodo(item)">
+                <span>
+                    status : {{ item.status }}
+                </span>
+                <s v-if="item.status == 'done'">
+                    {{ item.text }}
+                </s>
+                <span v-else>
+                    {{ item.text }}
+                </span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                todo : "",
+                items : []
+            }
+        },
+        methods: {
+            sendTodo() {
+                this.items.push({
+                    text : this.todo,
+                    status : 'todo'
+                })
+                this.todo = ""
+            },
+            doneTodo(item) {
+                item.status === 'done' ? item.status = 'todo' : item.status = 'done'
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
